0612fc991dfbf48be14f46fb458c49347d1801c8
wajde <fadoolwajdi@gmail.com>
2025-10-07 13:14:59 +0300

    working on board

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -7,8 +7,8 @@ const asyncHandler = require('../middleware/async')
 // @access  Private
 exports.getBoards = asyncHandler(async (req, res, next) => {
   // find boards where user is owner OR a member
-  console.log(req.user.id)
-  console.log(req.user.id)
+  // console.log(req.user.id)
+  // console.log(req.user.id)
   const boards = await Board.find({
     $or: [{ owner: req.user.id }, { 'members.userId': req.user.id }],
     // will get all boards where in the table of boards it have the user.id as the owner or the user.id is in the members array in the table
@@ -25,36 +25,6 @@ exports.getBoards = asyncHandler(async (req, res, next) => {
     success: true,
     count: boards.length,
     data: boards,
-  })
-})
-
-/// @desc   Get single board by ID
-// @route   GET /api/v1/boards/:id
-// @access  Private
-exports.getBoard = asyncHandler(async (req, res, next) => {
-  const board = await Board.findById(req.params.id)
-    .populate('owner', 'name email')
-    .populate('members.userId', 'name email')
-
-  if (!board) {
-    return next(
-      new ErrorResponse(`Board not found with id ${req.params.id}`, 404)
-    )
-  }
-
-  // check if user is owner or member
-  const isMember =
-    board.owner._id.equals(req.user.id) ||
-    board.members.some((m) => m.userId._id.equals(req.user.id))
-
-  // TODO: maby remove to middleware
-  if (!isMember) {
-    return next(new ErrorResponse('Not authorized to view this board', 403))
-  }
-
-  res.status(200).json({
-    success: true,
-    data: board,
   })
 })
 
@@ -89,36 +59,84 @@ exports.createBoard = asyncHandler(async (req, res, next) => {
   })
 })
 
+/*
+{
+    "success": true,
+    "data": {
+        "is_private": true,
+        "is_active": true,
+        "_id": "68e215b226f953da6caecd30",
+        "title": "main board Version 1 ",
+        "owner": "68dfe916a6e211bad9669d13",
+        "members": [
+            {
+                "userId": "68dfe916a6e211bad9669d13",
+                "role": "admin",
+                "_id": "68e215b226f953da6caecd31"
+            }
+        ],
+        "labels": [],
+        "createdAt": "2025-10-05T06:52:34.362Z",
+        "updatedAt": "2025-10-07T08:50:19.850Z",
+        "__v": 0,
+        "description": "this is description for the main board"
+    }
+}
+*/
+/// @desc   Get single board by ID
+// @route   GET /api/v1/boards/:id
+// @access  Private
+exports.getBoard = asyncHandler(async (req, res, next) => {
+  console.log('called get board')
+  // TODO: add indexing for faster request, add reddis , add cookies
+  const board = await Board.findOne({
+    _id: req.params.id,
+    $or: [{ owner: req.user.id }, { 'members.userId': req.user.id }],
+  })
+  // .populate('owner', 'name email')
+  // .populate('members.userId', 'name email')
+
+  // const board = await Board.findById(req.params.id)
+  //   .populate('owner', 'name email')
+  //   .populate('members.userId', 'name email')
+
+  // if (!board) {
+  //   return next(
+  //     new ErrorResponse(`Board not found with id ${req.params.id}`, 404)
+  //   )
+  // }
+
+  // check if user is owner or member
+  // const isMember =
+  //   board.owner._id.equals(req.user.id) ||
+  //   board.members.some((m) => m.userId._id.equals(req.user.id))
+
+  // TODO: maby remove to middleware
+  // if (!isMember) {
+  //   return next(new ErrorResponse('Not authorized to view this board', 403))
+  // }
+
+  res.status(200).json({
+    success: true,
+    data: board,
+  })
+})
+
 /// @desc   Update board
 // @route   PUT /api/v1/boards/:id
-// @access  Private
+// @access  Private (Only owner can update Board)
 exports.updateBoard = asyncHandler(async (req, res, next) => {
-  let board = await Board.findById(req.params.id)
-
-  //   TODO: fetch Board before hand
-  if (!board) {
-    return next(
-      new ErrorResponse(`Board not found with id ${req.params.id}`, 404)
-    )
-  }
-
-  // only owner can update TODO: move to middleware
-  if (board.owner.toString() !== req.user.id) {
-    return next(new ErrorResponse('Not authorized to update this board', 403))
-  }
-
-  const fieldsToUpdate = {
-    title: req.body.title || board.title,
-    description: req.body.description || board.description,
-    labels: req.body.labels || board.labels,
-    updatedAt: Date.now(),
-  }
-
-  board = await Board.findByIdAndUpdate(req.params.id, fieldsToUpdate, {
-    new: true,
-    runValidators: true,
+  // Fetch Board and make user
+  let board = await Board.findOne({
+    _id: req.params.id,
+    owner: req.user.id,
   })
 
+  board.title = req.body.title || board.title
+  board.description = req.body.description || board.description
+  board.labels = req.body.labels || board.labels
+
+  await board.save()
   res.status(200).json({
     success: true,
     data: board,
@@ -127,23 +145,13 @@ exports.updateBoard = asyncHandler(async (req, res, next) => {
 
 /// @desc   Delete board
 // @route   DELETE /api/v1/boards/:id
-// @access  Private
+// @access  Private (Only owner can delete Board)
 exports.deleteBoard = asyncHandler(async (req, res, next) => {
-  const board = await Board.findById(req.params.id)
-
-  if (!board) {
-    return next(
-      new ErrorResponse(`Board not found with id ${req.params.id}`, 404)
-    )
-  }
-
-  // only owner can delete
-  if (board.owner.toString() !== req.user.id) {
-    return next(new ErrorResponse('Not authorized to delete this board', 403))
-  }
-
+  let board = await Board.findOne({
+    _id: req.params.id,
+    owner: req.user.id,
+  })
   await board.deleteOne()
-
   res.status(200).json({
     success: true,
     message: 'Board deleted successfully',
--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -7,12 +7,14 @@ const {
   updateBoard,
 } = require('../controllers/boardController')
 const { protect } = require('../middleware/auth')
-
+const { checkAccess } = require('../middleware/checkAccess')
+const { authorizeBoardAccess } = require('../middleware/authorizeBoardAccess')
 const router = express.Router()
 
 router.route('/').get(protect, getBoards)
-router.route('/:id').get(protect, getBoard)
 router.route('/').post(protect, createBoard)
+
+router.route('/:id').get(protect, getBoard)
 router.route('/:id').delete(protect, deleteBoard)
 router.route('/:id').put(protect, updateBoard)
 
